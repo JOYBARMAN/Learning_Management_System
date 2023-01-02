68eb55ea549ea79615b5d983fcf810a8a1f7e152
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2023-01-02 17:51:53 +0600

    create techer api get/detail/put/delete

--- lms_backend_api/views.py
+++ lms_backend_api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from .serializers import TeacherSerializers
+from .models import Teacher
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+class TeacherList(APIView):
+    """
+    List all teacher, or create a new teacher.
+    """
+    def get(self, request, format=None):
+        teacher = Teacher.objects.all()
+        serializer = TeacherSerializers(teacher, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = TeacherSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TeacherDetail(APIView):
+    """
+    Retrieve, update or delete a teacher instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        teacher = self.get_object(pk)
+        serializer = TeacherSerializers(teacher)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        teacher = self.get_object(pk)
+        serializer = TeacherSerializers(teacher, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        teacher = self.get_object(pk)
+        teacher.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include('lms_backend_api.urls'))
 ]
 
 if settings.DEBUG:
